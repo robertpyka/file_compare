fa97f48e5e12b44812064f175b5ab2fceb6f5e09
robertpyka <rpyka.bytom@gmail.com>
2020-02-07 10:50:26 +0100

    Update dat_compare.py

--- dat_compare.py
+++ dat_compare.py
@@ -1,20 +1,41 @@
 
-#import
+#import GUI
 import tkinter as tk
 from tkinter import filedialog
-    
+from tkinter import *
+
+wel_wind = tk.Tk()
+
+#configuration of welcome window
+wel_wind.rowconfigure([0,1], minsize=10, weight=1)
+wel_wind.columnconfigure([0, 1, 2, 3, 4], minsize=10, weight=1)
+
+#adding components, 2 entries, 2 labes and 4 buttons
+f1_lab = tk.Label(wel_wind, text="Choose file 1")
+f1_lab.grid(row = 0, column = 0)
+f1_entry = tk.Entry()
+f1_entry.grid(row = 1, column = 0, padx = 5)
+f1_but = tk.Button(text ="...",width = 5, height = 1)
+f1_but.grid(row = 1, column = 1,padx=5)
+f2_lab = tk.Label(wel_wind, text="Choose file 2")
+f2_lab.grid(row = 2, column = 0)
+f2_entry = tk.Entry()
+f2_entry.grid(row = 3, column = 0, padx = 5)
+f2_but = tk.Button(text ="...",width = 5, height = 1)
+f2_but.grid(row = 3, column = 1,padx=5, pady = 5)
+comp_but = tk.Button(text = "Compare",width = 10, height = 1)
+comp_but.grid(row = 4, column = 1,padx=5, pady = 5, sticky = "w")
+clr_but = tk.Button(text = "Clear",width = 10, height = 1)
+clr_but.grid(row = 4, column = 0,padx=5, pady = 5, sticky = "e")
+
 def importA():
     #Take data to compare, dataA
 
-    #File dialog
-    root = tk.Tk()
-    root.withdraw()
-
-    file_path = filedialog.askopenfilename()
+    file_path = f1_entry.get()
     f = open(file_path, "r")
     dataA = f.readlines()
     #variable for new list
-    #loop for to create new list, with  useable elements at the begin 'DECL E6POS X'
+    #loop for to create new list, with  useable elements that begins with 'DECL E6POS X'
     a = 0
     for i in dataA:
         if i.count('DECL E6POS X') == 1:
@@ -28,16 +49,12 @@ def importA():
 
 def importB():
     #Take data to compare, dataB
-
-
-    root = tk.Tk()
-    root.withdraw()
-
-    file_path = filedialog.askopenfilename()
+    
+    file_path = f2_entry.get()
     f = open(file_path, "r")
     dataB = f.readlines()
     #variable for new list
-    #loop for to create new list, with  useable elements at the begin 'DECL E6POS X'
+    #loop for to create new list, with  useable elements that begins with 'DECL E6POS X'
     a = 0
     for i in dataB:
         if i.count('DECL E6POS X') == 1:
@@ -48,17 +65,59 @@ def importB():
     dataB = dataB[0:a]
     return dataB
 
-# main
-dataA = importA()
-dataB = importB()
-if dataA == dataB:
-    window = tk.Tk()
-    greeting = tk.Label(window, text="Files match")
-    greeting.pack()
-    window.mainloop()
+def f1_but_click(event):
+#Get filepath from user
 
-else:
-    window = tk.Tk()
-    greeting = tk.Label(window, text=dataB)
-    greeting.pack()
-    window.mainloop()
+    filename = filedialog.askopenfilename()
+    f1_entry.insert(0,filename)
+    return "break"
+#return "break" keeps button unsunken
+f1_but.bind("<Button-1>", f1_but_click)
+
+def f2_but_click(event):
+#Get filepath from user
+    
+    filename = filedialog.askopenfilename()
+    f2_entry.insert(0,filename)
+    return "break"
+f2_but.bind("<Button-1>", f2_but_click)
+
+def clr_but_click(event):
+#Clear all user inputs from entries
+    f1_entry.delete(0, tk.END)
+    f2_entry.delete(0, tk.END)
+clr_but.bind("<Button-1>", clr_but_click)
+
+def comp_but_click(event):
+    dataA = importA()
+    dataB = importB()
+    if dataA == dataB:
+        window = tk.Tk()
+        state = tk.Label(window, text="Files match")
+        state.pack()
+        window.mainloop()
+
+    else:
+        window = tk.Tk()
+        greeting = tk.Label(window, text=dataB)
+        greeting.pack()
+        window.mainloop()   
+comp_but.bind("<Button-1>", comp_but_click)
+wel_wind.mainloop()
+
+
+
+# main
+##dataA = importA()
+##dataB = importB()
+##if dataA == dataB:
+##    window = tk.Tk()
+##    greeting = tk.Label(window, text="Files match")
+##    greeting.pack()
+##    window.mainloop()
+##
+##else:
+##    window = tk.Tk()
+##    greeting = tk.Label(window, text=dataB)
+##    greeting.pack()
+##    window.mainloop()
